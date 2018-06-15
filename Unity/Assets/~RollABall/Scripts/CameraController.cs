40664aff42a88042ae41defd37120bf9bb931705
jacob-paisley <jacob.a.paisley@gmail.com>
2018-06-15 15:58:41 +1000

    Started Roll A Ball

---
+++ Unity/Assets/~RollABall/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RollABall
+{
+
+    public class CameraController : MonoBehaviour
+    {
+
+        public GameObject player;
+
+        private Vector3 offset;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            offset = transform.position - player.transform.position;
+        }
+
+        // LateUpdate to make sure the player has already moved for that frame
+        void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
